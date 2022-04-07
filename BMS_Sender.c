dd4c3f12210a30507b29554457accbe9f2378553
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-04-07 11:12:43 +0530

    Create BMS_Sender.c

---
+++ BMS_Sender.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "BMS_Sender.h"
+
+void readBMSDataFromFile(float* Temperature, float* SOC, float* ChargeRate)
+{
+  FILE* fp = fopen("./BMS_Parameters.txt","r");
+  float Temp_readings, SOC_readings, ChargeRate_readings;
+  int i;
+  if(fp == NULL)
+  {
+    printf("file not found\n");
+  }
+  else
+  {
+    for(i=0; fscanf(fp, "%f \t %f \t %f \n", &Temp_readings,&SOC_readings,&ChargeRate_readings)!=EOF; i++)
+    {
+      *(Temperature + i) = Temp_readings;
+      *(SOC + i) = SOC_readings;
+      *(ChargeRate + i) = ChargeRate_readings;
+    }
+  }
+  fclose(fp);
+}
+
+void printOnConsole(float TempPrint, float SOCPrint, float ChargeRatePrint)
+{
+  printf("Temperature: %.2f\t SOC: %.2f\t ChargeRate: %.2f\n", TempPrint,SOCPrint,ChargeRatePrint);
+}
+
+void sendBMSDataToConsole(float* Temperature, float* SOC, float* ChargeRate)
+{
+  float tempToPrint, SOCToPrint, chargeRateToPrint;
+  int i;
+  for(i=0;i<No_of_Readings; i++)
+  {
+    tempToPrint = *(Temperature + i);
+    SOCToPrint = *(SOC + i);
+    chargeRateToPrint = *(ChargeRate + i);
+    printOnConsole(tempToPrint,SOCToPrint,chargeRateToPrint);
+  }
+}
+
+void senderMain_BMS()
+{
+  float Temperature[No_of_Readings], SOC[No_of_Readings], ChargeRate[No_of_Readings] = {0};
+  readBMSDataFromFile(Temperature, SOC, ChargeRate);
+  sendBMSDataToConsole(Temperature, SOC, ChargeRate);  
+}
