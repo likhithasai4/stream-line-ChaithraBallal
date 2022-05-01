35a261a5f168568cc305de1750afb9a7aaafe266
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-01 23:24:28 +0530

    Update testSensorsDataReceiver.cpp

--- Receiver/testSensorsDataReceiver.cpp
+++ Receiver/testSensorsDataReceiver.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../test/catch.hpp"
-#include "Receiver/SensorsDataReceiver.h"
+#include "SensorsDataReceiver.h"
 
 TEST_CASE("read sensor data from console") {
   std::vector<SensorsData> sensorsDataHolder = readSensorsDataFromConsole();
