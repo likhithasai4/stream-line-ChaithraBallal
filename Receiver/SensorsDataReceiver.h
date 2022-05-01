777b61942c19e78d59286c9b0324f8fb24e5d1b8
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-01 23:12:30 +0530

    Update SensorsDataReceiver.h

--- Receiver/SensorsDataReceiver.h
+++ Receiver/SensorsDataReceiver.h
@@ -8,6 +8,7 @@ struct SensorsData{
   bool operator==(const SensorsData &other) const {
      if((temperatureSensorData==other.temperatureSensorData) && (SOCSensorData==other.SOCSensorData) && (chargeRate==other.chargeRate))
         return true;
+    else 
      return false;
     }
 };
