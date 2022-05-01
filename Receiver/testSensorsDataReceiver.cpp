a58a5b8201e625f4353de6a097a4934c84f33b11
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-01 23:55:00 +0530

    Update testSensorsDataReceiver.cpp

--- Receiver/testSensorsDataReceiver.cpp
+++ Receiver/testSensorsDataReceiver.cpp
@@ -6,4 +6,15 @@ TEST_CASE("read sensor data from console") {
   std::vector<SensorsData> sensorsDataHolder = readSensorsDataFromConsole();
   
   REQUIRE(sensorsDataHolder.size() == 50);
+  
+  struct SensorsData sensorsData;
+  sensorsData.temperatureSensorData = 78;
+  sensorsData.SOCSensorData = 56;
+  sensorsData.chargeRate = 0.97;
+  REQUIRE(sensorsDataHolder[44] == sensorsData);
+  
+  sensorsData.temperatureSensorData = 18;
+  sensorsData.SOCSensorData = 95;
+  sensorsData.chargeRate = 0.93;
+  REQUIRE(sensorsDataHolder[21] == sensorsData);
 }
