5e67c141eee059ef9de4f4534e0f30aefa852a88
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-01 23:07:17 +0530

    Create SensorsDataReceiver.cpp

---
+++ Receiver/SensorsDataReceiver.cpp
@@ -0,0 +1,16 @@
+#include "SensorsDataReceiver.h"
+#include <string>
+#include <cstdlib>
+
+std::vector<SensorsData> readSensorsDataFromConsole() {
+    std::vector<SensorsData> sensorsDataHolder;
+    SensorsData sensorsData;
+    std::string input;
+    getline(std::cin, input);
+    for(int i = 0; i < 50; i++) {
+      sensorsData.temperatureSensorData = atof(numberString.c_str() + 13);
+      sensorsData.SOCSensorData = atof(numberString.c_str() + 30);
+      sensorsData.chargeRate = atof(numberString.c_str() + 54);
+      sensorsDataHolder.push_back();
+    }
+}
