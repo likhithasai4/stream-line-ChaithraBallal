a39f6c19ba1bff8797c15c6fd772e7b6e32d8799
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-02 10:44:43 +0530

    Update testSensorsDataReceiver.cpp

--- Receiver/testSensorsDataReceiver.cpp
+++ Receiver/testSensorsDataReceiver.cpp
@@ -1,10 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "../test/catch.hpp"
 #include "SensorsDataReceiver.h"
-#include "../BMS_Sender.h"
 
 TEST_CASE("read sensor data from console") {
-  sender_BMS();
   std::vector<SensorsData> sensorsDataHolder = readSensorsDataFromConsole();
   
   REQUIRE(sensorsDataHolder.size() == 50);
