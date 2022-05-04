cd2462d86f35bcde827fed115559f0eed2891d72
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-04 22:51:49 +0530

    Update testSensorsDataReceiver.cpp

--- Receiver/testSensorsDataReceiver.cpp
+++ Receiver/testSensorsDataReceiver.cpp
@@ -2,25 +2,8 @@
 #include "../test/catch.hpp"
 #include "SensorsDataReceiver.h"
 
-TEST_CASE("read sensor data from console") {
-  std::vector<SensorsData> sensorsDataHolder = readSensorsDataFromConsole();
-  
-  REQUIRE(sensorsDataHolder.size() == 50);
-  
-  struct SensorsData sensorsData;
-  sensorsData.temperatureSensorData = 78;
-  sensorsData.SOCSensorData = 56;
-  sensorsData.chargeRate = 0.97;
-  REQUIRE(sensorsDataHolder[44] == sensorsData);
-  
-  sensorsData.temperatureSensorData = 18;
-  sensorsData.SOCSensorData = 95;
-  sensorsData.chargeRate = 0.93;
-  REQUIRE(sensorsDataHolder[21] == sensorsData);
-}
-
-
 TEST_CASE("validate statistics: Lowest") {
+  readSensorsDataFromConsole();
   struct SensorsData sensorsDataReceived = inferLowestValues();
   struct SensorsData sensorsDataExpected;
   sensorsDataExpected.temperatureSensorData = -15;
