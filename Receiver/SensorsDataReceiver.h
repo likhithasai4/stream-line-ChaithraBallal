4b4daff9395d667d3631299d2c67ff304b578c36
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-05-04 22:54:34 +0530

    Update SensorsDataReceiver.h

--- Receiver/SensorsDataReceiver.h
+++ Receiver/SensorsDataReceiver.h
@@ -8,6 +8,12 @@ struct SensorsData{
   bool operator==(const SensorsData &other) const {
     return ((temperatureSensorData==other.temperatureSensorData) && (SOCSensorData==other.SOCSensorData) && (chargeRate==other.chargeRate));
   }
+  
+  SensorsData() {
+    temperatureSensorData = 0.0;
+    SOCSensorData = 0.0;
+    chargeRate = 0.0;
+  }
 };
 
 std::vector<SensorsData> readSensorsDataFromConsole();
